2e7bc0c767367f5ca92424805fed40ea32b6644d
Nguyen Nguyen <nguyen.nguyen@ptnglobalcorp.com>
2022-01-24 18:43:28 +0700

    add input files

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require_relative './lib/application'
 
 begin
   Application.run!('inputs/input1.txt')
+  Application.run!('inputs/input2.txt')
+  Application.run!('inputs/input3.txt')
 rescue RuntimeError => e
   puts 'An eror occurs while running the application:'
   puts e.message
